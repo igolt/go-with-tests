0e83880601b6a414cc9c6e828b34bbfc56814e62
Igor Correa Rodrigues <igorcorrearodrigues@gmail.com>
2025-03-14 14:00:00 -0300

    feat: `SumAll`

--- arrays/sum_test.go
+++ arrays/sum_test.go
@@ -1,6 +1,9 @@
 package arrays
 
-import "testing"
+import (
+	"slices"
+	"testing"
+)
 
 func TestSum(t *testing.T) {
 	t.Run("collection of 5 numbers", func(t *testing.T) {
@@ -23,4 +26,13 @@ func TestSum(t *testing.T) {
 			t.Errorf("got %d expected %d given, %v", got, expected, numbers)
 		}
 	})
+}
+
+func TestSumAll(t *testing.T) {
+	got := SumAll([]int{1, 2}, []int{0, 9})
+	expected := []int{3, 9}
+
+	if !slices.Equal(got, expected) {
+		t.Errorf("got %d expected %d", got, expected)
+	}
 }
