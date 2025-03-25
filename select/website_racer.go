c9e59c9d86cfcb6d0d3acb1b785be12843b02e04
Igor Correa Rodrigues <igorcorrearodrigues@gmail.com>
2025-03-25 10:54:50 -0300

    feat: add timeout to our website racer

--- select/website_racer.go
+++ select/website_racer.go
@@ -1,15 +1,25 @@
 package website_racer
 
 import (
+	"fmt"
 	"net/http"
+	"time"
 )
 
-func Racer(url1, url2 string) (winner string) {
+const racerDefaultTimeout = 10 * time.Second
+
+func Racer(url1, url2 string) (winner string, error error) {
+	return ConfigurableRacer(url1, url2, racerDefaultTimeout)
+}
+
+func ConfigurableRacer(url1, url2 string, timeout time.Duration) (winner string, error error) {
 	select {
 	case <-ping(url1):
-		return url1
+		return url1, nil
 	case <-ping(url2):
-		return url2
+		return url2, nil
+	case <-time.After(timeout):
+		return "", fmt.Errorf("timed out waiting for %s and %s", url1, url2)
 	}
 }
 
--- select/website_racer_test.go
+++ select/website_racer_test.go
@@ -8,21 +8,38 @@ import (
 )
 
 func TestRacer(t *testing.T) {
-	slowServer := makeDelayedServer(10 * time.Microsecond)
-	fastServer := makeDelayedServer(0 * time.Microsecond)
+	t.Run("compare speed of servers, returning the url of the faster one", func(t *testing.T) {
+		slowServer := makeDelayedServer(10 * time.Millisecond)
+		fastServer := makeDelayedServer(0 * time.Millisecond)
 
-	defer slowServer.Close()
-	defer fastServer.Close()
+		defer slowServer.Close()
+		defer fastServer.Close()
 
-	slowURL := slowServer.URL
-	fastURL := fastServer.URL
+		slowURL := slowServer.URL
+		fastURL := fastServer.URL
 
-	expected := fastURL
-	got := Racer(slowURL, fastURL)
+		expected := fastURL
+		got, err := Racer(slowURL, fastURL)
+		if err != nil {
+			t.Fatalf("did not expect to get an error but got one %v", err)
+		}
 
-	if got != expected {
-		t.Errorf("got %q, expected %q", got, expected)
-	}
+		if got != expected {
+			t.Errorf("got %q, expected %q", got, expected)
+		}
+	})
+
+	t.Run("returns an error if a server doesn't respond within the specified time", func(t *testing.T) {
+		server := makeDelayedServer(5 * time.Millisecond)
+
+		defer server.Close()
+
+		_, err := ConfigurableRacer(server.URL, server.URL, time.Millisecond)
+
+		if err == nil {
+			t.Error("expected an error but didn't get one")
+		}
+	})
 }
 
 func makeDelayedServer(delay time.Duration) *httptest.Server {
