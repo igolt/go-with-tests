b2818a62f22e88252b946baf04b7b00ce0d51200
Igor Correa Rodrigues <igorcorrearodrigues@gmail.com>
2025-05-09 21:17:15 -0300

    refactor(blogposts): separate openning file from parsing file

--- reading-files/blogposts.go
+++ reading-files/blogposts.go
@@ -15,7 +15,7 @@ func NewPostsFromFS(fileSystem fs.FS) ([]Post, error) {
 
 	var posts []Post
 	for _, f := range dir {
-		post, err := getPost(fileSystem, f)
+		post, err := getPost(fileSystem, f.Name())
 		if err != nil {
 			return nil, err
 		}
@@ -24,18 +24,20 @@ func NewPostsFromFS(fileSystem fs.FS) ([]Post, error) {
 	return posts, nil
 }
 
-func getPost(fileSystem fs.FS, f fs.DirEntry) (Post, error) {
-	postFile, err := fileSystem.Open(f.Name())
+func getPost(fileSystem fs.FS, fileName string) (Post, error) {
+	postFile, err := fileSystem.Open(fileName)
 	if err != nil {
 		return Post{}, err
 	}
 	defer postFile.Close()
+	return newPostFromFile(postFile)
+}
 
-	postData, err := io.ReadAll(postFile)
+func newPostFromFile(file fs.File) (Post, error) {
+	postData, err := io.ReadAll(file)
 	if err != nil {
 		return Post{}, err
 	}
-
 	post := Post{Title: string(postData)[7:]}
 	return post, nil
 }
