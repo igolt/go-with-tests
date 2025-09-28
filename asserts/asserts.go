d3103109d70f9775380aeabb6486b748db1c9ce9
Igor Correa Rodrigues <igor.rodrigues@ssys.com.br>
2025-09-28 19:15:37 -0300

    refactor: move assert functions to a separate package

---
+++ asserts/asserts.go
@@ -0,0 +1,27 @@
+package asserts
+
+import "testing"
+
+func AssertTrue(t *testing.T, got bool) {
+	t.Helper()
+	AssertEqual(t, got, true)
+}
+
+func AssertFalse(t *testing.T, got bool) {
+	t.Helper()
+	AssertEqual(t, got, false)
+}
+
+func AssertEqual[T comparable](t *testing.T, got, expected T) {
+	t.Helper()
+	if expected != got {
+		t.Errorf("expected %v but got %v", expected, got)
+	}
+}
+
+func AssertNotEqual[T comparable](t *testing.T, got, expected T) {
+	t.Helper()
+	if expected == got {
+		t.Errorf("expected %v to be not equal to %v", expected, got)
+	}
+}
--- generics/generics_test.go
+++ generics/generics_test.go
@@ -3,6 +3,7 @@ package generics_test
 import (
 	"testing"
 
+	. "github.com/igolt/go-with-tests/asserts"
 	"github.com/igolt/go-with-tests/generics"
 )
 
@@ -67,28 +68,4 @@ func TestStack(t *testing.T) {
 		AssertEqual(t, value, "")
 		AssertFalse(t, ok)
 	})
-}
-
-func AssertTrue(t *testing.T, got bool) {
-	t.Helper()
-	AssertEqual(t, got, true)
-}
-
-func AssertFalse(t *testing.T, got bool) {
-	t.Helper()
-	AssertEqual(t, got, false)
-}
-
-func AssertEqual[T comparable](t *testing.T, got, expected T) {
-	t.Helper()
-	if expected != got {
-		t.Errorf("expected %v but got %v", expected, got)
-	}
-}
-
-func AssertNotEqual[T comparable](t *testing.T, got, expected T) {
-	t.Helper()
-	if expected == got {
-		t.Errorf("expected %v to be not equal to %v", expected, got)
-	}
 }
